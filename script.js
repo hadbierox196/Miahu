9b1db87aae8cb4036c024fb209a4a6079559ab1a
hadbierox196 <143258343+hadbierox196@users.noreply.github.com>
2024-12-04 10:24:33 +0500

    Update script.js

--- script.js
+++ script.js
@@ -5,26 +5,31 @@ function showSlides() {
     const slides = document.getElementsByClassName("slides");
     const dots = document.getElementsByClassName("dot");
 
+    // Hide all slides
     for (let i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
 
+    // Increment slide index
     slideIndex++;
     if (slideIndex > slides.length) {
         slideIndex = 1;
     }
 
+    // Remove "active" class from all dots
     for (let i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", "");
     }
 
+    // Show current slide and set corresponding dot as active
     slides[slideIndex - 1].style.display = "block";
     dots[slideIndex - 1].className += " active";
 
-    setTimeout(showSlides, 5000); // Change image every 5 seconds
+    // Change image every 5 seconds
+    setTimeout(showSlides, 5000);
 }
 
 function currentSlide(n) {
-    slideIndex = n - 1;
+    slideIndex = n - 1; // Set slide index to chosen slide
     showSlides();
 }
